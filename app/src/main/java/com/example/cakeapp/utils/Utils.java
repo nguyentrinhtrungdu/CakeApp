5a64262eaa7e618ffd2180a37fa4d7d57dbd75f6
Du <du@gmail.com>
2024-08-23 21:28:26 +0700

    s

--- app/src/main/java/com/example/cakeapp/utils/Utils.java
+++ app/src/main/java/com/example/cakeapp/utils/Utils.java
@@ -1,6 +1,8 @@
 package com.example.cakeapp.utils;
 
 public class Utils {
-    public static final String BASE_URL="http://172.16.119.186/banhang/";
-
+    public static final String BASE_URL="http://10.0.2.2/banhang/";
 }
+
+
+

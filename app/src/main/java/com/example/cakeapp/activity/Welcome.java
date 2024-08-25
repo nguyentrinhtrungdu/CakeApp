f55027860104f0c67403497e94757f6292c90402
Du <du@gmail.com>
2024-08-25 23:21:57 +0700

    koko

---
+++ app/src/main/java/com/example/cakeapp/activity/Welcome.java
@@ -0,0 +1,33 @@
+package com.example.cakeapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.cakeapp.R;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+
+    }
+}
--- app/src/main/java/com/example/cakeapp/retrofit/ApiApp.java
+++ app/src/main/java/com/example/cakeapp/retrofit/ApiApp.java
@@ -3,6 +3,7 @@ package com.example.cakeapp.retrofit;
 
 import com.example.cakeapp.model.LoaiSpModel;
 import com.example.cakeapp.model.SanPhamMoiModel;
+import com.example.cakeapp.model.UserModel;
 
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.Field;
@@ -22,6 +23,14 @@ public interface ApiApp {
     Observable<SanPhamMoiModel> getSanPham(
       @Field("page") int page,
       @Field("loai") int loai
+    );
+    @POST("dangki.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangKi(
+            @Field("email") String email,
+            @Field("pass") String pass,
+            @Field("username") String username,
+            @Field("num") String num
     );
 
 }
